cf78e52ea831db7011e947e465bdd8a4a23915bb
Patryk Kopiczko <95355@student.pb.edu.pl>
2026-01-11 15:23:09 +0100

    Utworzone podstawy programu, pliki szkieletowe i niektóre funkcje: -Tworzenie rejestru -Dodawanie i modyfikacja postaci -Wyszukiwanie Nie napotkałem błędów w działaniu programu.

---
+++ bohater.h
@@ -0,0 +1,39 @@
+#ifndef BOHATER_H
+#define BOHATER_H
+
+#define IMIE_DLUGOSC 100
+#define TEKST_DLUGOSC 50
+struct Rejestr;
+typedef struct Rejestr Rejestr;
+typedef enum {
+    AKTYWNY,
+    NA_MISJI,
+    RANNY,
+    ZAGINIONY,
+    ZAWIESZONY
+} enumStatus;
+
+typedef enum {
+    WOJOWNIK,
+    MAG,
+    KAPLAN,
+    LOTRZYK,
+    LOWCA,
+    DRUID
+} enumKlasa;
+
+typedef struct {
+    char imie[IMIE_DLUGOSC];
+    char rasa[TEKST_DLUGOSC];
+    enumKlasa klasa;
+    int poziom;
+    int reputacja;
+    enumStatus status;
+} Bohater;
+void nowyBohatera(Bohater *b);
+void atrybutyBohatera(Bohater *b);
+void zmienBohatera(Rejestr *r);
+char* statusNaTekst(enumStatus status);
+char* klasaNaTekst(enumKlasa klasa);
+
+#endif
---
+++ rejestr.h
@@ -0,0 +1,26 @@
+#ifndef REJESTR_H
+#define REJESTR_H
+
+#include "bohater.h"
+
+typedef struct Rejestr {
+    Bohater *bohaterowie;
+    int rozmiar;
+    int pojemnosc;
+} Rejestr;
+typedef struct {
+    char imie[IMIE_DLUGOSC];
+    char rasa[TEKST_DLUGOSC];
+    int klasa;
+    int minpoziom;
+    int maxpoziom;
+    int minreputacja;
+    int maxreputacja;
+    int status;
+} Filtry;
+void dodajRejestr(Rejestr* r);
+void wklejBohatera(Rejestr* r, Bohater b);
+void ustawFiltry(Filtry* f);
+void filtruj(Rejestr* r, Filtry* f, Rejestr* wynik);
+void wyswietlBohaterow(Rejestr* r);
+#endif
